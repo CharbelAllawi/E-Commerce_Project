db913ac00c0be8a273cf313ea9c909fdc96e4640
Charbel Allawi <charbelallawiwork@gmail.com>
2023-07-31 22:38:59 +0300

    addorupdateproduct crudcontroller

--- Ecommerce-Server/app/Http/Controllers/CRUDController.php
+++ Ecommerce-Server/app/Http/Controllers/CRUDController.php
@@ -15,4 +15,28 @@ class CRUDController extends Controller
 
 
   }
+  public function addOrUpdateProduct(Request $request, $id = "add")
+  {
+    if ($id == "add") {
+      $product = new Product;
+    } else {
+      $product = Product::where('product_id', $id)->first();
+    }
+
+    $product->category_id = $request->category_id ? $request->category_id : $product->category_id;
+    $product->name = $request->name ? $request->name : $product->name;
+    $product->description = $request->description ? $request->description : $product->description;
+    $product->price = $request->price ? $request->price : $product->price;
+    $product->color = $request->color ? $request->color : $product->color;
+
+    if ($request->hasFile('imageurl')) {
+      $file_name = time() . "product_image" . "." . $request->imageurl->extension();
+      $request->imageurl->move(storage_path('images'), $file_name);
+      $product->imageurl = storage_path("images") . "\\" . $file_name;
+    }
+
+    $product->save();
+
+    return json_encode(["products" => $product]);
+  }
 }
